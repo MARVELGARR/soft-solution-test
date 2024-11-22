2acb0e6a46f5e3b9832c9996c7f88feb537ea1e9
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2024-11-22 14:19:02 +0100

    m

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,5 +1,6 @@
+import { prisma } from '@/prisma/prisma';
 import { NextResponse } from 'next/server';
-import { prisma } from '../../../prisma/prisma';
+
 
 
 export async function POST(req: Request){
