9d8c771d8d237f153240a9ea4dca9aa2822cfbf9
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2024-11-22 12:18:53 +0100

    next build fix

--- app/api/[todo]/route.ts
+++ app/api/[todo]/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/prisma/prisma";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req: Request, { params }: { params: { todo: string } }) {
+export async function DELETE(req: Request, res: Response, { params }: { params: { todo: string } }) {
   try {
     // Delete the todo by its id (passed in params.todo)
     const deletedTodo = await prisma.todo.delete({
